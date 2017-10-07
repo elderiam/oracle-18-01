fa1ce051c8dcfd0d5e4227f5774b76a56a826034
elderiam <elder_iam@live.com.mx>
2017-10-07 09:46:45 -0500

    Add files via upload

--- Curso_BD2.sql
+++ Curso_BD2.sql
@@ -2,4 +2,7 @@
 
 
 --Clase de hoy 7/10/17
-create table almacen(id_almacen integer,ubicacion_almacen varchar(80), constraint pk_id_almacen primary key(id_almacen));
+create table almacen(id_almacen integer,
+                    ubicacion_almacen varchar(80), 
+                    constraint pk_id_almacen primary key(id_almacen)
+                    );
