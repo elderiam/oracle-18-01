f5b16aeb5e4f63700fb9fe1f126876bf9443c0ff
elderiam <elder_iam@live.com.mx>
2017-10-07 10:18:45 -0500

    Add files via upload

--- Curso_BD2.sql
+++ Curso_BD2.sql
@@ -2,7 +2,26 @@
 
 
 --Clase de hoy 7/10/17
-create table almacen(id_almacen integer,
+create table almacen(numero_almacen integer,
                     ubicacion_almacen varchar(80), 
-                    constraint pk_id_almacen primary key(id_almacen)
+                    constraint pk_numero_almacen primary key(numero_almacen)
                     );
+                    
+                    
+--Una entidaad o registro tipo alamacen
+create or replace procedure guardar_almacen( mi_id IN integer,
+                            mi_ubicacion In varchar2)
+                            as
+                            begin
+                            --Aqui la logica del procedimiento
+                    insert into almacen values(mi_id, mi_ubicacion);
+                    end;
+                    /
+--Vamos a probar que funciona nuestro procedimiento
+    begin
+    guardar_almacen(1,'PLYMOUTH');
+    end;
+    /
+    
+    select * from almacen;
+    
